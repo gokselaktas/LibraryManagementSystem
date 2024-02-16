6d18eaf655f74127c998241c9002f8c640563743
Goksel Aktas <aktas_goksel@hotmail.com>
2024-02-16 13:44:01 +0000

    task completed.

--- Library.py
+++ Library.py
@@ -4,19 +4,42 @@ class Library:
 
     def liste_kitap(self):
         #kitap listelenecek kitap ismi yazari sayfa sayisi ? baska ne olabilir.
-        return null
+        # Go to the start of the file
+        self.file.seek(0)
+        # Read all lines and split them into a list
+        lines = self.file.read().splitlines()
+        # Print book names and authors
+        for line in lines:
+            book_info = line.split(',')
+            print(f"Title: {book_info[0]}, Author: {book_info[1]}")
 
     def ekle_kitap(self):
         # kitap icin ne kadar bilgi tutulacaksa o kadar kullanicidan input iste
         # kitap ismi ve yazari kesinlikle olmali
         # diger degerleri bos birakabilir mi kullanici?
-        return null
+        title = input("Enter book title: ")
+        author = input("Enter author name: ")
+        pages = input("Enter number of pages: ")
+        book_str = f"{title},{author},{pages}\n"
+        #String olusturduk
+        self.file.write(book_str)
+        self.file.flush()
 
     def sil_kitap(self):
         # kitap ismini sor ve direkt o kitaba ait butun bilgileri sil
         # kitap ismi unique degilse 2. degiskeni sor (yazari)
         # mevcut butun kitaplari sildigimizde books.txt'ye bir yazi belki...
-        return null
+        title_to_remove = input("Enter the title of the book to remove: ")
+        self.file.seek(0)
+        lines = self.file.read().splitlines()
+        # kitabi bul
+        lines = [line for line in lines if not line.startswith(title_to_remove)]
+        # Clear the file and rewrite the list
+        self.file.seek(0)
+        self.file.truncate()
+        for line in lines:
+            self.file.write(f"{line}\n")
+        self.file.flush()
     
 
     #basit bir GUI yapilabilir mi.
@@ -25,22 +48,22 @@ class Library:
 if __name__ == "__main__":
     lib = Library()  # Library Sınıfından lib nesnesi tanımlandı.
 
-    while True:
+while True:
         print("*** MENU ***")
-        print("1) Kitapları listele")
-        print("2) Kitap Ekleme")
-        print("3) Kitap Silme")
+        print("1) List Books")
+        print("2) Add Book")
+        print("3) Remove Book")
         #ekstra fonksiyon gelebilir mi?
-        print("q) Çıkış")
-        kullaniciInput = input()
+        print("q) Exit")
+        choice = input()
 
-        if kullaniciInput == 'q':
+        if choice == 'q':
             break
-        elif kullaniciInput == '1':
+        elif choice == '1':
             lib.liste_kitap()
-        elif kullaniciInput == '2':
+        elif choice == '2':
             lib.ekle_kitap()
-        elif kullaniciInput == '3':
+        elif choice == '3':
             lib.sil_kitap()
         else:
-            print("Yanlış girdiniz.")
+            print("Invalid choice.")
