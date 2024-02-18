72719a3abc02629dffbd96a671e5518592e16e84
Goksel Aktas <aktas_goksel@hotmail.com>
2024-02-18 01:04:26 +0000

    MainWindow.py Upgraded.

--- MainWindow.py
+++ MainWindow.py
@@ -1,17 +1,19 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
+import os
 import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow, QListView
 from PyQt5.QtGui import QStandardItemModel, QStandardItem
 
-class FileManager:
-    def __init__(self, filename='books.txt'):
+
+class FileManager: # Dosya islemleri icin FileManager sinifi olusturuldu
+    def __init__(self, filename='books.txt'): # Dosya adı belirtilmezse varsayılan olarak books.txt dosyası oluşturacak
         self.filename = filename
 
-    def append_to_file(self, content):
+    def append_to_file(self, content): # Dosyaya kitap ekleyen fonksiyon
         with open(self.filename, 'a+') as file:
             file.write(content)
 
-    def read_books_from_file(self):
+    def read_books_from_file(self): # Dosyadan kitapları okuyan fonksiyon
         with open(self.filename, 'r') as file:
             lines = file.read().splitlines()
         return lines
@@ -32,58 +34,58 @@ class FileManager:
 
 class Ui_MainWindow(object):
     FileManager = FileManager()
-    #FileManager.append_to_file("GlobalAI, Python, 2021, 300")
-
 
     def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
         MainWindow.resize(641, 687)
+        MainWindow.setWindowTitle("Library Management System") # Pencere basligi
+        MainWindow.setWindowIcon(QtGui.QIcon('./icons/globalaihub.jpeg')) # GlobalAIHub logosu eklendi
 
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton.setGeometry(QtCore.QRect(30, 30, 121, 131))
-        self.pushButton.setObjectName("pushButton")
+        self.pushButton.setIcon(QtGui.QIcon("./icons/globalaihub.jpeg")) # GlobalAIHub logosu eklendi
+        self.pushButton.setIconSize(QtCore.QSize(121, 131)) # icon boyutu ayarlamasi
+        self.pushButton.clicked.connect(lambda: os.system("start https://globalaihub.com/"))# GlobalAIHub logosuna tıklanınca siteye yönlendirme yapiyor.
+        # sertifika aldigimda oraya yonlendirecegim :)
 
         self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_2.setGeometry(QtCore.QRect(30, 180, 121, 131))
-        self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_2.setIcon(QtGui.QIcon('./icons/list.png')) # Listeleme logosu eklendi
+        self.pushButton_2.setIconSize(QtCore.QSize(121, 131)) #icon boyutu ayarlamasi
 
         self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_3.setGeometry(QtCore.QRect(30, 330, 121, 131))
-        self.pushButton_3.setObjectName("pushButton_3")
+        self.pushButton_3.setIcon(QtGui.QIcon('./icons/add.png')) # Ekleme logosu eklendi
+        self.pushButton_3.setIconSize(QtCore.QSize(121, 131)) #icon boyutu ayarlamasi
 
         self.pushButton_4 = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_4.setGeometry(QtCore.QRect(30, 480, 121, 131))
-        self.pushButton_4.setObjectName("pushButton_4")
+        self.pushButton_4.setIcon(QtGui.QIcon('./icons/delete.png')) #Silme logosu eklendi
+        self.pushButton_4.setIconSize(QtCore.QSize(121, 131)) #icon boyutu ayarlamasi
 
         self.listView = QtWidgets.QListView(self.centralwidget)
         self.listView.setGeometry(QtCore.QRect(225, 31, 391, 541))
-        self.listView.setObjectName("listView")
 
         self.pushButton_5 = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_5.setGeometry(QtCore.QRect(520, 590, 93, 28))
-        self.pushButton_5.setObjectName("pushButton_5")
+        self.pushButton_5.setText("Exit") # Exit butonu eklendi
 
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 641, 26))
-        self.menubar.setObjectName("menubar")
 
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
 
-        self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
         self.pushButton_2.clicked.connect(self.load_books_into_listview)
-        self.pushButton_3.clicked.connect(self.AddWindow)# add window fonksiyonu eklenecek
-        self.pushButton_4.clicked.connect(self.DelWindow)# del window fonksiyonu eklenecek
-        self.pushButton_5.clicked.connect(lambda:self.closeScreen())# exit butonuna tıklanınca
+        self.pushButton_3.clicked.connect(self.AddWindow) # Ekleme ekrani cagiriliyor
+        self.pushButton_4.clicked.connect(self.DelWindow) # Silme ekrani cagiriliyor
+        self.pushButton_5.clicked.connect(lambda:self.ExitFunction())# exit butonuna tıklanınca
 
     def load_books_into_listview(self):
-        # Dosyadan kitapları okuyun
         books = self.FileManager.read_books_from_file()  # Dosyadan kitapları okuyan fonksiyon
         
         # Model tabanli bir widget kullandigimiz icin bir model olusturuyoruz
@@ -96,11 +98,8 @@ class Ui_MainWindow(object):
         
         # Modeli ListView'a bağladık
         self.listView.setModel(model)
-        
 
-        
-
-    def closeScreen(self):# exit butonuna tıklanınca her şeyi kapatır.
+    def ExitFunction(self):# exit butonuna tıklanınca her şeyi kapatır.
         sys.exit()
 
     def AddWindow(self):# kitap eklemek için çıkacak ekran
@@ -109,7 +108,6 @@ class Ui_MainWindow(object):
         self.ui = Ui_Form()
         self.ui.setupUi(self.window)
         self.window.show()
-        print("")
 
     def DelWindow(self): # kitap silmek için çıkacak ekran	
         from DelWindow import Ui_Form
@@ -118,18 +116,7 @@ class Ui_MainWindow(object):
         self.ui.setupUi(self.window)
         self.window.show()
 
-
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "Library Management System"))
-        self.pushButton.setText(_translate("MainWindow", "GlobalAI LOGO"))
-        self.pushButton_2.setText(_translate("MainWindow", "List Books"))
-        self.pushButton_3.setText(_translate("MainWindow", "Add Book"))
-        self.pushButton_4.setText(_translate("MainWindow", "Delete Book"))
-        self.pushButton_5.setText(_translate("MainWindow", "Exit"))
-
-
-if __name__ == "__main__":
+if __name__ == "__main__": # ekranı çalıştırır
     import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
